bfbb994213ebb7106ce7e0cdec5e9128e73b4523
Trotyl <trotyl@qq.com>
2017-08-06 22:45:59 +0800

    add test case

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, IterableDiffers } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  viewProviders: [
+    IterableDiffers.extend([])
+  ]
 })
 export class AppComponent {
   title = 'app';
